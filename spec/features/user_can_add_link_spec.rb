27f9c61ce3683c1b3a506d1b8d03490d39ae1f7c
Charlotte Moore <mooc123@gmail.com>
2017-05-22 10:41:40 -0600

    added feature test for adding link

---
+++ spec/features/user_can_add_link_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "when user is on root/links index they see a form to submit new link" do
+  it "when they fill out form with valid url and title they are able to submit a new link" do
+    user = User.create(email: 'email@email.com', password: 'pass')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit root_path
+
+    within('#link-form') do
+      fill_in "Url", with: 'https://www.google.com/'
+      fill_in "Title", with: 'Google'
+
+      click_on('Add link')
+    end
+
+    expect(current_path).to eq(root_path)
+
+    within('#link-list') do
+      expect(page).to have_content('https://www.google.com/')
+      expect(page).to have_content('Google')
+      expect(page).to have_button('Mark as Read')
+      expect(page).to have_button('Edit')
+    end
+  end
+end
