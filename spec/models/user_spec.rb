c2cc188bb0bdb817c7c85adcc58a759e75be5c79
Charlotte Moore <mooc123@gmail.com>
2017-05-22 09:57:32 -0600

    added model tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "secure password" do
+    it { should have_secure_password }
+  end
+
+  context "validations" do
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it {should validate_presence_of(:password)}
+    it {should validate_uniqueness_of(:email)}
+  end
 end
