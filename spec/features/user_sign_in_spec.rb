872100d2e5c24c8d09983ac19cfaa7f552b68564
Charlotte Moore <mooc123@gmail.com>
2017-05-22 10:12:27 -0600

    added sad path for invalid credentials

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -20,4 +20,23 @@ describe "when guest user visits root they are redirected to signup page" do
     expect(current_path).to eq(root_path)
     expect(page).to have_content('email@email.com has successfully logged in')
   end
+
+  it "they cannot login with invalid credentials" do
+    user = User.create(email: 'email@email.com', password: 'pass')
+
+    visit root_path
+
+    expect(current_path).to eq(signup_path)
+
+    click_on("Login")
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "Email", with: 'email@email.com'
+    fill_in 'Password', with: 'password'
+
+    click_on("Login")
+
+    expect(page).to have_content("User Email or Password invalid")
+  end
 end
