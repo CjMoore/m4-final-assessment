84a62b7001813e897270d251227f619ebf6e3097
Charlotte Moore <mooc123@gmail.com>
2017-05-22 13:31:44 -0600

    added sad path for non user owned link on load

--- spec/features/users_links_on_load_spec.rb
+++ spec/features/users_links_on_load_spec.rb
@@ -5,6 +5,7 @@ describe "when logged in user visits root", js: true do
     user = User.create(email: 'email@email.com', password: 'pass')
     link1 = user.links.create(title: 'Google', url: 'http://google.com')
     link2 = user.links.create(title: 'Reddit', url: 'http://reddit.com')
+    link3 = Link.create(title: 'Wut', url: 'http://wut.com')
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -16,6 +17,8 @@ describe "when logged in user visits root", js: true do
       expect(page).to have_content("Reddit")
       expect(page).to have_link('http://google.com')
       expect(page).to have_link('http://reddit.com')
+      expect(page).to_not have_content('Wut')
+      expect(page).to_not have_link('http://wut.com')
     end
   end
 end
